646c5a5fba9d72e4f7373eec0f2092b04fdf2b0e
BBC <986444785@qq.com>
2016-07-08 15:40:24 +0800

    Initial Commit

---
+++ RitaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RitaApp
+//
+//  Created by BBC on 16/7/8.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RitaApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RitaApp
+//
+//  Created by BBC on 16/7/8.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
